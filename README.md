a55a3ae73739b8128f23fe51214b06076c06bdad
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-06-07 15:26:01 +0200

    Upload: Optionally (re)start the instance
    - Add default site configuration
    - Start parameterized instance
    - Clean up and document

--- README.md
+++ README.md
@@ -25,6 +25,12 @@ Fill in the `BIG_BOAT` domain name (such that it is your dashboard's base URL)
 and the `API_KEY` (obtainable from the dashboard's configuration when logged in 
 or replace the like with `v1: true` to use the old API).
 
+You can add multiple sites in your settings, all of which are considered by 
+default unless you specify specific ones when running `upload.py`. Ensure you 
+leave no empty lines between the blocks. Additionally, a setting block for 
+a site called default may provide global settings. The global settings may be 
+overridden (for simple values) or augmented (for associative values).
+
 Edit `docker-compose.yml` and `bigboat-compose.yml` to alter or add generic 
 configuration of the Docker instances. Environment variables are used by the 
 scraper (startup) scripts within Docker and by the configuration scripts in it, 
@@ -32,3 +38,8 @@ thus the environment variable names matter. Use parameters enclosed by double
 curly brackets to make the compose file parameterized such that it can be 
 configured per-site at activation time. This is useful for e.g. credentials 
 which are not stored *in plain sight*.
+
+The upload script can also start an instance for the application on the site 
+that was just updated. The parameter values can be specified in `settings.yml` 
+with a `params` associative value. It is common practice to set unused values 
+to a dash character, but make sure it is quoted in the YAML file.
--- upload.py
+++ upload.py
@@ -9,6 +9,7 @@ except ImportError:
     raise
 
 import argparse
+import copy
 import itertools
 import logging
 import yaml
@@ -31,7 +32,9 @@ def parse_args():
     parser.add_argument('--version', default='1',
                         help='Version number of the application')
     parser.add_argument('--log', choices=log_levels, default='INFO',
-                        help='log level (info by default)')
+                        help='Log level (info by default)')
+    parser.add_argument('--start', action='store_true', default=False,
+                        help='(Re)start an instance after updating the app')
 
     return parser.parse_args()
 
@@ -73,7 +76,7 @@ class Uploader(object):
         if key is None:
             client = Client_v1
 
-        logging.info('Setting up API for %s: %s', self._site, repr(client))
+        logging.info('Setting up API for %s', self._site)
 
         self._api = client(self._site, api_key=key)
         return self._api
@@ -99,6 +102,55 @@ class Uploader(object):
                                                compose_file.read()):
                     raise RuntimeError('Cannot update compose file on {}'.format(self._site))
 
+    def start(self, name, version):
+        """
+        Request that the default instance for the application is (re)started.
+        """
+
+        parameters = self._options.get('parameters', {})
+
+        instance = self.api.update_instance(name, name, version,
+                                            parameters=parameters)
+        if instance is None:
+            raise RuntimeError('Could not start instance on {}'.format(self._site))
+
+        logging.info('Started application %s version %s on %s', name, version,
+                     self._site)
+
+def run(args, site, options):
+    """
+    Perform the upload for a single site.
+    """
+
+    uploader = Uploader(site, **options)
+    uploader.upload(args.name, args.version)
+    if args.start:
+        uploader.start(args.name, args.version)
+
+DEFAULT_SITE = 'default'
+
+def get_options(config, site):
+    """
+    Create an options dictionary based on configuration for a site as well as
+    a default configuration.
+
+    The two configurations are merged such that the site-specific values
+    override default values if they are simple, and associative values do the
+    same (only one level deep).
+    """
+
+    options = copy.deepcopy(config.get(DEFAULT_SITE, {}))
+    new_options = config.get(site, {})
+    for key, value in list(new_options.iteritems()):
+        if isinstance(value, dict):
+            new_value = options.get(key, {})
+            new_value.update(value)
+            options[key] = new_value
+        else:
+            options[key] = value
+
+    return options
+
 def main():
     """
     Main entry point.
@@ -115,17 +167,17 @@ def main():
         logging.info('Sites: %s', ', '.join(args.sites))
         # pylint: disable=no-member
         for site, key in itertools.zip_longest(args.sites, args.keys):
-            options = config.get(site, {})
+            options = get_options(config, site)
             if key is not None:
                 options['key'] = key
 
-            uploader = Uploader(site, **options)
-            uploader.upload(args.name, args.version)
+            run(args, site, options)
     else:
-        logging.info('Default sites: %s', ', '.join(config.keys()))
-        for site, options in list(config.items()):
-            uploader = Uploader(site, **options)
-            uploader.upload(args.name, args.version)
+        sites = [site for site in config.keys() if site != DEFAULT_SITE]
+        logging.info('Sites: %s', ', '.join(sites))
+        for site in sites:
+            options = get_options(config, site)
+            run(args, site, options)
 
 if __name__ == '__main__':
     main()
